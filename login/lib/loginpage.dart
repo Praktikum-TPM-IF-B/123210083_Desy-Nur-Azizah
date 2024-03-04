3cabf73b0e1b9082c8b64d0873327e5284297ea2
desyna <123210083@student.upnyk.ac.id>
2024-03-05 00:20:27 +0700

    fush

---
+++ login/lib/loginpage.dart
@@ -0,0 +1,104 @@
+import 'package:flutter/material.dart';
+
+class LoginPage extends StatelessWidget {
+  const LoginPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        margin: const EdgeInsets.all(24),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: [
+            _header(),
+            _inputField(),
+            _forgotPassword(),
+            _signup(),
+          ],
+        ),
+      ),
+    );
+  }
+
+  _header() {
+    return const Column(
+      children: [
+        Text(
+          "Login",
+          style: TextStyle(fontSize: 40, fontWeight: FontWeight.bold),
+        ),
+        Text("Hello, welcome back"),
+      ],
+    );
+  }
+
+  _inputField() {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.stretch,
+      children: [
+        TextField(
+          decoration: InputDecoration(
+              hintText: "Username",
+              border: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(18),
+                  borderSide: BorderSide.none),
+              fillColor: Colors.blueAccent.withOpacity(0.1),
+              filled: true,
+              prefixIcon: const Icon(Icons.person)),
+        ),
+        const SizedBox(height: 10),
+        TextField(
+          decoration: InputDecoration(
+            hintText: "Password",
+            border: OutlineInputBorder(
+                borderRadius: BorderRadius.circular(18),
+                borderSide: BorderSide.none),
+            fillColor: Colors.blueAccent.withOpacity(0.1),
+            filled: true,
+            prefixIcon: const Icon(Icons.password),
+          ),
+          obscureText: true,
+        ),
+        const SizedBox(height: 10),
+        ElevatedButton(
+          onPressed: () {},
+          style: ElevatedButton.styleFrom(
+            shape: const StadiumBorder(),
+            padding: const EdgeInsets.symmetric(vertical: 16),
+            backgroundColor: Colors.blue,
+          ),
+          child: const Text(
+            "Login",
+            style: TextStyle(fontSize: 20, color: Colors.white),
+          ),
+        )
+      ],
+    );
+  }
+
+  _forgotPassword() {
+    return TextButton(
+      onPressed: () {},
+      child: const Text(
+        "Forgot password?",
+        style: TextStyle(color: Colors.blueGrey),
+      ),
+    );
+  }
+
+  _signup() {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        const Text("Dont have an account? "),
+        TextButton(
+            onPressed: () {},
+            child: const Text(
+              "Sign Up",
+              style: TextStyle(color: Colors.blueGrey),
+            ))
+      ],
+    );
+  }
+}
